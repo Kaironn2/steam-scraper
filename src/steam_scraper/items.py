a7db1fb4e623a2fdf00e5fee710abd962af05d65
kaironn2 <jonathas_oliveira17@hotmail.com>
2026-07-12 17:23:01 -0300

    feat: add user_achievements spider

--- src/steam_scraper/items.py
+++ src/steam_scraper/items.py
@@ -7,3 +7,16 @@ class Game(scrapy.Item):
     appid = scrapy.Field()
     name = scrapy.Field()
     achievements_total = scrapy.Field()
+
+
+class Achievement(scrapy.Item):
+    username = scrapy.Field()
+    appid = scrapy.Field()
+    game = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    unlocked = scrapy.Field()
+    unlock_time = scrapy.Field()
+    progress_current = scrapy.Field()
+    progress_total = scrapy.Field()
+    language = scrapy.Field()
